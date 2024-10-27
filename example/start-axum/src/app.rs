1e08ba24a0a9865909b3570054aa452cabcfc90a
Jan Dryk <me@uxsoft.cz>
2024-10-27 21:48:50 +0100

    update examples to 0.7

--- example/start-axum/src/app.rs
+++ example/start-axum/src/app.rs
@@ -1,8 +1,26 @@
 use crate::error_template::{AppError, ErrorTemplate};
-use leptos::*;
+use leptos::prelude::*;
 use leptos_image::{provide_image_context, Image};
 use leptos_meta::*;
-use leptos_router::*;
+use leptos_router::{components::*, path};
+
+pub fn shell(options: LeptosOptions) -> impl IntoView {
+    view! {
+        <!DOCTYPE html>
+        <html lang="en">
+            <head>
+                <meta charset="utf-8"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1"/>
+                <AutoReload options=options.clone() />
+                <HydrationScripts options/>
+                <MetaTags/>
+            </head>
+            <body>
+                <App/>
+            </body>
+        </html>
+    }
+}
 
 #[component]
 pub fn App() -> impl IntoView {
@@ -13,82 +31,67 @@ pub fn App() -> impl IntoView {
     view! {
         <Stylesheet id="leptos" href="/pkg/start-axum.css"/>
         <Title text="Welcome to Leptos"/>
-        <Router fallback=|| {
-            let mut outside_errors = Errors::default();
-            outside_errors.insert_with_default_key(AppError::NotFound);
-            view! { <ErrorTemplate outside_errors/> }.into_view()
-        }>
+        <Router>
+            <div
+                style:display="flex"
+                style:width="40rem"
+                style:justify-content="space-between"
+                style:margin-left="auto"
+                style:margin-right="auto">
+                <div>
+                    <a href="/">"Home"</a>
+                </div>
+                <div>
+                    <a href="/lg">"Large"</a>
+                </div>
+                <div>
+                    <a href="/md">"Medium"</a>
+                </div>
+                <div>
+                    <a href="/sm">"Small"</a>
+                </div>
+                <div>
+                    <a href="/no-blur">"No Blur"</a>
+                </div>
+            </div>
             <main>
-                <Routes>
+                <FlatRoutes fallback=|| "Not found.">
+
                     <Route
-                        path=""
+                        path=path!("/")
                         view=|| {
-                            view! {
-                                <div
-                                    style:display="flex"
-                                    style:width="40rem"
-                                    style:justify-content="space-between"
-                                    style:margin-left="auto"
-                                    style:margin-right="auto"
-                                >
-                                    <div>
-                                        <a href="/">"Home"</a>
-                                    </div>
-                                    <div>
-                                        <a href="/lg">"Large"</a>
-                                    </div>
-                                    <div>
-                                        <a href="/md">"Medium"</a>
-                                    </div>
-                                    <div>
-                                        <a href="/sm">"Small"</a>
-                                    </div>
-                                    <div>
-                                        <a href="/no-blur">"No Blur"</a>
-                                    </div>
-                                </div>
-                                <Outlet/>
-                            }
+                            view! { <h1>"Welcome to Leptos Image"</h1> }
                         }
-                    >
+                    />
 
-                        <Route
-                            path="/"
-                            view=|| {
-                                view! { <h1>"Welcome to Leptos Image"</h1> }
-                            }
-                        />
+                    <Route
+                        path=path!("/lg")
+                        view=|| {
+                            view! { <ImageComparison width=1000 height=1000 blur=true/> }
+                        }
+                    />
 
-                        <Route
-                            path="/lg"
-                            view=|| {
-                                view! { <ImageComparison width=1000 height=1000 blur=true/> }
-                            }
-                        />
+                    <Route
+                        path=path!("/md")
+                        view=|| {
+                            view! { <ImageComparison width=500 height=500 blur=true/> }
+                        }
+                    />
 
-                        <Route
-                            path="/md"
-                            view=|| {
-                                view! { <ImageComparison width=500 height=500 blur=true/> }
-                            }
-                        />
+                    <Route
+                        path=path!("/sm")
+                        view=|| {
+                            view! { <ImageComparison width=100 height=100 blur=true/> }
+                        }
+                    />
 
-                        <Route
-                            path="/sm"
-                            view=|| {
-                                view! { <ImageComparison width=100 height=100 blur=true/> }
-                            }
-                        />
-
-                        <Route
-                            path="/no-blur"
-                            view=|| {
-                                view! { <ImageComparison width=1000 height=1000 blur=false/> }
-                            }
-                        />
-
-                    </Route>
-                </Routes>
+                    <Route
+                        path=path!("/no-blur")
+                        view=|| {
+                            view! { <ImageComparison width=1000 height=1000 blur=false/> }
+                        }
+                    />
+                </FlatRoutes>
             </main>
         </Router>
     }
@@ -109,7 +112,7 @@ fn ImageComparison(width: u32, height: u32, blur: bool) -> impl IntoView {
                 <div>
                     <h1>{format!("Optimized ({width} x {height}) with blur preview")}</h1>
                 </div>
-                <Image src="/cute_ferris.png" width height quality=85 blur class="test-image"/>
+                <Image src="/cute_ferris.png" width height quality=85 blur class:test-image=true/>
             </div>
             <div>
                 <div>
--- example/start-axum/src/main.rs
+++ example/start-axum/src/main.rs
@@ -2,38 +2,29 @@
 #[tokio::main]
 async fn main() {
     use axum::{routing::post, Router};
-    use leptos::*;
-    use leptos_axum::{generate_route_list, handle_server_fns, LeptosRoutes};
+    use leptos::prelude::*;
+    use leptos_axum::{file_and_error_handler, generate_route_list, handle_server_fns, LeptosRoutes};
     use leptos_image::*;
     use start_axum::app::*;
-    use start_axum::fileserv::file_and_error_handler;
     use tokio::net::TcpListener;
 
     // Composite App State with the optimizer and leptos options.
     #[derive(Clone, axum::extract::FromRef)]
     struct AppState {
-        leptos_options: leptos::LeptosOptions,
+        leptos_options: LeptosOptions,
         optimizer: leptos_image::ImageOptimizer,
     }
-    simple_logger::init_with_level(log::Level::Info).expect("couldn't initialize logging");
+    // simple_logger::init_with_level(log::Level::Info).expect("couldn't initialize logging");
 
-    // Setting get_configuration(None) means we'll be using cargo-leptos's env values
-    // For deployment these variables are:
-    // <https://github.com/leptos-rs/start-axum#executing-a-server-on-a-remote-machine-without-the-toolchain>
-    // Alternately a file can be specified such as Some("Cargo.toml")
-    // The file would need to be included with the executable when moved to deployment
-    let conf = get_configuration(None).await.unwrap();
+    let conf = get_configuration(Some("Cargo.toml")).unwrap();
     let leptos_options = conf.leptos_options;
     let addr = leptos_options.site_addr;
     let routes = generate_route_list(App);
-
-    let conf = get_configuration(None).await.unwrap();
-    let leptos_options = conf.leptos_options;
     let root = leptos_options.site_root.clone();
 
     let state = AppState {
         leptos_options,
-        optimizer: ImageOptimizer::new("/cache/image", root, 1),
+        optimizer: ImageOptimizer::new("/cache/image", root.to_string(), 1),
     };
 
     // Build Router.
@@ -43,13 +34,13 @@ async fn main() {
         .image_cache_route(&state)
         // Provide the optimizer to leptos context.
         .leptos_routes_with_context(&state, routes, state.optimizer.provide_context(), App)
-        .fallback(file_and_error_handler)
+        .fallback(file_and_error_handler(shell))
         .with_state(state);
 
     // run our app with hyper
     // `axum::Server` is a re-export of `hyper::Server`
     let listener = TcpListener::bind(&addr).await.unwrap();
-    logging::log!("listening on http://{}", &addr);
+    // logging::log!("listening on http://{}", &addr);
     axum::serve(listener, app.into_make_service())
         .await
         .unwrap();
